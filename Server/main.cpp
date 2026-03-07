c522aeb887b76c0f3ff49ef699c616f2d521fe11
idanoha <idan.ohayon@campus.technion.ac.il>
2026-03-07 19:04:12 +0200

    Add pdh and CPU usage monitoring

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,10 +1,42 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <windows.h>
 #include <sysinfoapi.h>
+#include <iomanip>
+#include <pdh.h>
 #define GB_SIZE (1024.0*1024.0*1024.0)
 
-void handleCommand(const std::string& cmd) {
+static PDH_HQUERY cpuQuery;
+static PDH_HCOUNTER cpuTotal;
+
+/* Creates a PDH query and adds relevant performance counter for monitoring cpu usage
+ */
+void initCpuCounter(){
+    PdhOpenQueryW(NULL, 0, &cpuQuery);
+    // You can also use L"\\Processor(*)\\% Processor Time" and get individual CPU values with PdhGetFormattedCounterArray()
+    PdhAddEnglishCounterW(cpuQuery, L"\\Processor(_Total)\\% Processor Time", 0, &cpuTotal);
+    PdhCollectQueryData(cpuQuery);
+}
+
+/* Samples performance data regarding cpu usage and returning formatted value in percentage
+ */
+double getCurrentCPUUsage(){
+    PDH_FMT_COUNTERVALUE counterVal;
+
+    PdhCollectQueryData(cpuQuery);
+    PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
+    return counterVal.doubleValue;
+}
+
+void trim(std::string &str) { // Note: also deletes interior whitespaces, could be bad for future commands
+    std::string::iterator end_pos = std::remove(str.begin(), str.end(), ' ');
+    str.erase(end_pos, str.end());
+}
+
+void handleCommand(const std::string& input) {
+    std::string cmd = input;
+    trim(cmd);
     if (cmd == "EXIT") {
         std::exit(0);
     }
@@ -19,20 +51,25 @@ void handleCommand(const std::string& cmd) {
             double totalInGB = state.ullTotalPhys / GB_SIZE;
             double usedInGB = totalInGB - state.ullAvailPhys / GB_SIZE;
             DWORD percentage = state.dwMemoryLoad;
-            std:: cout << "RAM In Use: " << usedInGB << "GB / " << totalInGB <<
+            std::cout << std::fixed << std::setprecision(1);
+            std:: cout << "RAM In Use: " << usedInGB << " GB / " << totalInGB <<
                 " GB (" << percentage << "%)" << std::endl;
             std::cout << "RAM Available: " << availInGB << " GB" << std::endl;
         }
+    } else if (cmd == "CPU") {
+        std::cout << "CPU Usage: " << getCurrentCPUUsage() << "%" << std::endl;
+
     } else {
         std::cout << "Unknown command: '" << cmd << "'. Try again" << std::endl;
     }
 }
 
 int main() {
+    initCpuCounter();
     std::string command;
 
     while (true) {
-        std::cout << "Memory> ";
+        std::cout << "Monitor> ";
         getline(std::cin, command);
         handleCommand(command);
     }
