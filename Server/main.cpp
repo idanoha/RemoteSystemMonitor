bc5ff9641dfaca4a98f2705f4bd2ed0f9cddd6fb
idanoha <idan.ohayon@campus.technion.ac.il>
2026-03-07 13:48:13 +0200

    Add basic command for loop

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,3 +1,26 @@
-//
-// Created by idano on 3/7/2026.
-//
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+void handleCommand(std::string command) {
+    if (command == "EXIT") {
+        std::exit(0);
+    } else if (command == "RAM") {
+
+    } else {
+        std::cout << "Unknown command: " << command << ". Try again" << std::endl;
+    }
+
+}
+
+int main() {
+    std::string command;
+
+    while (true) {
+        std::cout << "Memory> ";
+        getline(std::cin, command);
+        handleCommand(command);
+        break;
+    }
+    return 0;
+}
